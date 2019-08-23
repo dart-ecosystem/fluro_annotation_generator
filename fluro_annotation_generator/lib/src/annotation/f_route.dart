71bbaa83495c874e5db823c03334db000f9a703c
copydog <775150@qq.com>
2019-08-23 15:58:03 +0800

    structure: move generator to dedicate folder

--- fluro_annotation_generator/lib/src/annotation/f_route.dart
+++ fluro_annotation_generator/lib/src/annotation/f_route.dart
@@ -1,4 +1,5 @@
-import 'package:fluro/src/common.dart' show HandlerType, HandlerFunc;
+import 'package:fluro/src/common.dart'
+    show HandlerType, HandlerFunc, TransitionType;
 
 import 'base_annotation.dart';
 
@@ -6,10 +7,12 @@ class FRoute extends FRouteBase {
   final String url;
   final HandlerType handlerType;
   final HandlerFunc handlerFunc;
+  final TransitionType transitionType;
 
   const FRoute(
     this.url, {
     this.handlerType,
     this.handlerFunc,
+    this.transitionType,
   });
 }
